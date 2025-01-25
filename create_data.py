1b85ad260f94446be5078ce5533b819f8cf95028
Meet soni <meetsoni1998@gmail.com>
2025-01-25 18:10:30 +0530

    Add data creation and track with DVC

---
+++ create_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+# Generate the synthetic dataset
+np.random.seed(42)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+data = pd.DataFrame(np.hstack((X, y)), columns=['X', 'y'])
+
+# Save the dataset
+data.to_csv('data.csv', index=False)
+print("Dataset created and saved to data.csv")
