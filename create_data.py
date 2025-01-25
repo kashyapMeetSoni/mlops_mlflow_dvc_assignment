36d42ba103c631f864a7d45c048023c5efeba228
Meet soni <meetsoni1998@gmail.com>
2025-01-25 18:13:15 +0530

    Update dataset with new random seed

--- create_data.py
+++ create_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Generate the synthetic dataset
-np.random.seed(42)
+np.random.seed(100)
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.randn(100, 1)
 data = pd.DataFrame(np.hstack((X, y)), columns=['X', 'y'])
